b7480c1f5d1ec6b49881578c750f94b2b0ed3391
GiharaNavindu <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2025-08-07 09:41:28 +0530

    page_changed

--- syncFusionTest/src/App.tsx
+++ syncFusionTest/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { ColumnDirective, ColumnsDirective, GridComponent } from '@syncfusion/ej2-react-grids';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
 
 function App() {
   const data = [
@@ -14,16 +15,29 @@ function App() {
     }
   ];
   return (
-    <GridComponent dataSource={data}>
-      <ColumnsDirective>
+
+
+    <div>
+      <GridComponent dataSource={data}>
+        <ColumnsDirective>
           <ColumnDirective field='OrderID' width='100' textAlign="Right"/>
           <ColumnDirective field='CustomerID' width='100'/>
           <ColumnDirective field='EmployeeID' width='100' textAlign="Right"/>
           <ColumnDirective field='Freight' width='100' format="C2" textAlign="Right"/>
           <ColumnDirective field='OrderDate' width='100' format="yMd" textAlign="Right"/>
           <ColumnDirective field='ShipCountry' width='100'/>
-      </ColumnsDirective>
-    </GridComponent>
+        </ColumnsDirective>
+      </GridComponent>
+      <div>
+        <DatePickerComponent placeholder='Select a date' format='dd/MM/yyyy format' />
+        <DatePickerComponent placeholder='Select a date' format='dd/MM/yyyy' value={new Date(2023, 9, 1)} />
+        <DatePickerComponent placeholder='Select a date' format='dd/MM/yyyy' value={new Date(2023, 9, 15)} />
+      </div>
+    </div>
+    
+    
+
+    
   );
 }
 
